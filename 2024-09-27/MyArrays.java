0a0acb486458831326a59a22baa58bf6d1f2ef03
Jeff Ou <jou60@stuy.edu>
2024-09-29 23:30:23 -0400

    Added formatting for test cases for concatArray()

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -2,15 +2,21 @@ public class MyArrays{
   public static void main(String[] args){
     System.out.println("Test Cases for arytoString():");
 
-    System.out.println("[1, 2, 3, 4, 5]" + " vs my " + aryToString(new int[]{1, 2, 3, 4, 5}));
-    System.out.println("[0]" + " vs my " + aryToString(new int[]{0}));
-
+    System.out.println("[1, 2, 3, 4, 5] vs my " + aryToString(new int[]{1, 2, 3, 4, 5}));
+    System.out.println("[0] vs my " + aryToString(new int[]{0}));
+// ----------------------------------------------------------------------------------------------
     System.out.println("\nTest Cases for returnCopy():"); 
 
     int[] ary = new int[]{1};
     boolean matchAry = (aryToString(ary).equals(aryToString(returnCopy(ary))));
     boolean Address = (ary != returnCopy(ary));
     System.out.println(aryToString(ary) + " Matching Arrays: " + matchAry + " / Different Addresses: " + Address);
+//-----------------------------------------------------------------------------------------------
+    System.out.println("\nTest Cases for concatArray():");
+
+    int[] ary1 = new int[]{1};
+    int[] ary2 = new int[]{2};
+    System.out.println("[1, 2] vs my " + aryToString(concatArray(ary1, ary2)));
   }
 
   public static String aryToString(int[] nums){
@@ -26,5 +32,9 @@ public class MyArrays{
 
   public static int[] returnCopy(int[] ary){
     return new int[1]; //insert cool code here
+  }
+
+  public static int[] concatArray(int[]ary1, int[]ary2){
+    return new int[1];
   }
 }
