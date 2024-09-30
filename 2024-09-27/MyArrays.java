e2fdb23efe005c0c1352b8fcf1fe36ddf990e6a8
Jeff Ou <jou60@stuy.edu>
2024-09-29 23:59:26 -0400

    Added concatArray()

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -91,6 +91,13 @@ public class MyArrays{
   }
 
   public static int[] concatArray(int[]ary1, int[]ary2){
-    return new int[1];
+    int[] newAry = new int[ary1.length + ary2.length];
+    for (int i = 0; i < ary1.length; i++){
+      newAry[i] = ary1[i];
+    }
+    for (int i = ary1.length; i < newAry.length; i++){
+      newAry[i] = ary2[i - ary1.length];
+    }
+    return newAry;
   }
 }
