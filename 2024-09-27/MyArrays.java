5892a2dd320c4b5e1e34db7d080bd38f3c25ac30
Jeff Ou <jou60@stuy.edu>
2024-09-29 23:46:59 -0400

    Added test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -4,19 +4,71 @@ public class MyArrays{
 
     System.out.println("[1, 2, 3, 4, 5] vs my " + aryToString(new int[]{1, 2, 3, 4, 5}));
     System.out.println("[0] vs my " + aryToString(new int[]{0}));
+    System.out.println("[] vs my " + aryToString(new int[]{}));
 // ----------------------------------------------------------------------------------------------
     System.out.println("\nTest Cases for returnCopy():"); 
 
-    int[] ary = new int[]{1};
+    int[] ary = new int[]{0};
     boolean matchAry = (aryToString(ary).equals(aryToString(returnCopy(ary))));
     boolean Address = (ary != returnCopy(ary));
-    System.out.println(aryToString(ary) + " Matching Arrays: " + matchAry + " / Different Addresses: " + Address);
+    System.out.println("Matching Arrays: " + matchAry + " / Different Addresses: " + Address + " / " + aryToString(ary));
+
+    ary = new int[]{1};
+     matchAry = (aryToString(ary).equals(aryToString(returnCopy(ary))));
+     Address = (ary != returnCopy(ary));
+    System.out.println("Matching Arrays: " + matchAry + " / Different Addresses: " + Address + " / " + aryToString(ary));
+
+    ary = new int[]{1, 2, 3, 4, 5};
+     matchAry = (aryToString(ary).equals(aryToString(returnCopy(ary))));
+     Address = (ary != returnCopy(ary));
+    System.out.println("Matching Arrays: " + matchAry + " / Different Addresses: " + Address + " / " + aryToString(ary));
+
+    ary = new int[]{23, 54, 0, 11, 2};
+     matchAry = (aryToString(ary).equals(aryToString(returnCopy(ary))));
+     Address = (ary != returnCopy(ary));
+    System.out.println("Matching Arrays: " + matchAry + " / Different Addresses: " + Address + " / " + aryToString(ary));
+
+    ary = new int[]{};
+     matchAry = (aryToString(ary).equals(aryToString(returnCopy(ary))));
+     Address = (ary != returnCopy(ary));
+    System.out.println("Matching Arrays: " + matchAry + " / Different Addresses: " + Address + " / " + aryToString(ary));
+
 //-----------------------------------------------------------------------------------------------
     System.out.println("\nTest Cases for concatArray():");
 
     int[] ary1 = new int[]{1};
     int[] ary2 = new int[]{2};
     System.out.println("[1, 2] vs my " + aryToString(concatArray(ary1, ary2)));
+
+    ary1 = new int[]{1};
+    ary2 = new int[]{1};
+    System.out.println("[1, 1] vs my " + aryToString(concatArray(ary1, ary2)));
+
+    ary1 = new int[]{1, 20, 3};
+    ary2 = new int[]{4, 50, 6};
+    System.out.println("[1, 20, 3, 4, 50, 6] vs my " + aryToString(concatArray(ary1, ary2)));
+
+    ary1 = new int[]{10, 20, 30};
+    ary2 = new int[]{40, 50};
+    System.out.println("[10, 20, 30, 40, 50] vs my " + aryToString(concatArray(ary1, ary2)));
+
+    ary1 = new int[]{60, 70};
+    ary2 = new int[]{80, 90, 100};
+    System.out.println("[60, 70, 80, 90, 100] vs my " + aryToString(concatArray(ary1, ary2)));
+
+    ary1 = new int[]{};
+    ary2 = new int[]{10, 20, 30};
+    System.out.println("[10, 20, 30] vs my " + aryToString(concatArray(ary1, ary2)));
+
+    ary1 = new int[]{40, 50, 60};
+    ary2 = new int[]{};
+    System.out.println("[40, 50, 60] vs my " + aryToString(concatArray(ary1, ary2)));
+
+    ary1 = new int[]{};
+    ary2 = new int[]{};
+    System.out.println("[] vs my " + aryToString(concatArray(ary1, ary2)));
+
+
   }
 
   public static String aryToString(int[] nums){
