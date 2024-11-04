7c5942d04695b50e17ef47871bc46b732cdd8d70
Jiefeng Ou <jou60@stuy.edu>
2024-11-04 10:33:46 -0500

    changed name of inputTxt

--- 10-31-Advent/Driver.java
+++ 10-31-Advent/Driver.java
@@ -1,5 +1,5 @@
 public class Driver{
 public static void main(String[]args){
-    System.out.println(TriangleTester.countTrianglesA("InputTri.txt"));
+    System.out.println(TriangleTester.countTrianglesA("inputTri.txt"));
 }
 }
---
+++ 2024-10-15-point/Demo.java
@@ -0,0 +1,40 @@
+public class Demo{
+
+  /*
+  Write this method third. Test it then move on
+  to the others.
+  */
+  public static double distance(Point a, Point b){
+    return (Math.sqrt((Math.pow(a.getX() - b.getX(), 2.0) + Math.pow(a.getY() - b.getY(), 2.0))));
+  }
+
+  public static void main(String[]args){
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(-1,-1);
+    Point p3 = new Point(3,4);
+
+    System.out.println( p1);
+    System.out.println( p2);
+    System.out.println( p3);
+
+    System.out.println( distance(p2,p1));
+    System.out.println( Point.distance(p1,p2));
+    System.out.println( p2.distanceTo(p1));
+
+    System.out.println( distance(p1,p3));
+    System.out.println( Point.distance(p3,p1));
+    System.out.println( p1.distanceTo(p3));
+
+    System.out.println( distance(p3,p2));
+    System.out.println( Point.distance(p2,p3));
+    System.out.println( p3.distanceTo(p2));
+
+    p1 = new Point(0, 0);
+    p2 = new Point(1, 0);
+    p3 = new Point(1.0/2, Math.sqrt(3) / 2);
+
+    System.out.println(distance(p1, p2));
+    System.out.println(p1.distanceTo(p3));
+    System.out.println(Point.distance(p2, p3));
+  }
+}
