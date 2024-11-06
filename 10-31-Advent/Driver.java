3694ad20d4ce6e32f8dc44f82d2c8a879ad1bb41
Jeff Ou <jou60@stuy.edu>
2024-11-06 05:18:22 -0500

    Added countTrianglesB from class

--- 10-31-Advent/Driver.java
+++ 10-31-Advent/Driver.java
@@ -1,5 +1,6 @@
 public class Driver{
 public static void main(String[]args){
     System.out.println(TriangleTester.countTrianglesA("inputTri.txt"));
+    System.out.println(TriangleTester.countTrianglesB("inputTri.txt"));
 }
 }
