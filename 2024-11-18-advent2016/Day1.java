6ad0cfff3a6a8165b61e650318f03dff5cf98b8a
Jiefeng Ou <jou60@stuy.edu>
2024-11-19 10:55:11 -0500

    Finished Day1 Part1

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Day1{
   public static void main(String[] args){
@@ -8,13 +9,36 @@ public class Day1{
       File day1 = new File("day1.txt");
       Scanner sc = new Scanner(day1);
 
-      int xcoor = 0;
-      int ycoor = 0;
+      int x = 0, y = 0, facing = 0;
 
       while(sc.hasNext()){
         String input = sc.next();
-        System.out.println(input);
+        String[] step = new String[]{input.substring(0,1), input.substring(1,input.length() - 1)};
+
+        if (step[0].equals("L")){
+          facing = (facing + 3) % 4;
+        }
+        else{
+          facing = (facing + 1) % 4;
+        }
+
+        int distance = Integer.parseInt(step[1]);
+        if (facing == 0){
+          y += distance;
+        }
+        else if (facing == 1){
+          x += distance;
+        }
+        else if (facing == 2){
+          y -= distance;
+        }
+        else if (facing == 3){
+          x -= distance;
+        }
+
       }
+
+      System.out.println(Math.abs(x) + Math.abs(y));
       sc.close();
 
     }catch(FileNotFoundException e){
