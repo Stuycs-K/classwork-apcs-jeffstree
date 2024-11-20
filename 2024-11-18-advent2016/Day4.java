eb575fd8a259b47af9caa64c96c834f230fa10a5
Jeff Ou <jou60@stuy.edu>
2024-11-20 05:29:31 -0500

    Day4: Array of amount of each character

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -10,9 +10,30 @@ public class Day4{
       Scanner sc = new Scanner(day4);
 
       int sum = 0;
+      String alpha = "abcdefghijklmnopqrstuvwxyz";
+      String[] alphabet = alpha.split("");
+
       while(sc.hasNext()){
         String input = sc.next();
-        System.out.println(input);
+        String[] inputArr = input.split("-");
+
+        String encryptedName = "";
+        for (int i = 0; i < inputArr.length - 1; i++){
+            encryptedName += inputArr[i];
+        }
+        int sectorID = Integer.parseInt(inputArr[inputArr.length - 1].substring(0, 3));
+        String checksum = inputArr[inputArr.length - 1].substring(4, 9);
+
+        int[] count = new int[26];
+        for(int i = 0; i < count.length; i++){ //loops 26 times for each letter
+            String[] nameArr = encryptedName.split("");
+            for(int letter = 0; letter < nameArr.length; letter++){
+                if (nameArr[letter].equals(alphabet[i])){
+                    count[i] ++;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(count));
       }
       sc.close();
 
