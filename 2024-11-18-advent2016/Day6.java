9688d7333ad01cd590715dc139c8d612bee09102
Jiefeng Ou <jou60@stuy.edu>
2024-11-20 11:01:08 -0500

    Day6: Unfinished

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -1,16 +1,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Day6{
   public static void main(String[] args){
     try{
       File day6 = new File("day6.txt");
       Scanner sc = new Scanner(day6);
+
+      ArrayList<String> rows = new ArrayList<String>();
       while(sc.hasNext()){
-        System.out.println(sc.next());
+        rows.add(sc.next());
       }
+
+      //TURN ROWS INTO COLUMNS
+      ArrayList<String> columns = new ArrayList<String>();
+      for(int i = 0; i < rows.size(); i++){
+        String value = rows.get(i);
+        //WORK HERE
+      }
+
+      String alpha = "abcdefghijklmnopqrstuvwxyz";
+      String[] alphabet = alpha.split("");
+      int[] count = new int[26];
+      for(int i = 0; i < count.length; i++){ //loops 26 times for each letter
+        for(int j = 0; j < columns.size(); j++){ //loops through every string in input
+          //TURN ROWS INTO COLUMNS, THEN WORK ON THIS
+        }
+      }
+
       sc.close();
 
     }catch(FileNotFoundException e){
