6ffcb08463003120cb5fcf7b725233693b8ea7af
Jeff Ou <jou60@stuy.edu>
2024-11-19 19:17:56 -0500

    Day2: Made array of the instructions

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -8,6 +8,15 @@ public class Day2{
     try{
       File day2 = new File("day2.txt");
       Scanner sc = new Scanner(day2);
+      int[][] keypad = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+      while(sc.hasNext()){
+        String input = sc.next();
+        String[] movement = input.split("");
+        System.out.println(Arrays.toString(movement));
+        int x = 1, y = 1;
+        
+
+      }
       
       sc.close();
 
