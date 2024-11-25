a1db5faa8a1693711bedb765ae5af06c08e90aeb
Jeff Ou <jou60@stuy.edu>
2024-11-24 23:39:34 -0500

    Day6 Part 1 finished

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Day6{
@@ -14,22 +15,41 @@ public class Day6{
         rows.add(sc.next());
       }
 
-      //TURN ROWS INTO COLUMNS
+      //turns rows into columns
       ArrayList<String> columns = new ArrayList<String>();
-      for(int i = 0; i < rows.size(); i++){
-        String value = rows.get(i);
-        //WORK HERE
+      for(int i = 0; i < rows.get(0).length(); i++){
+        String columnValue = "";
+        for(int j = 0; j < rows.size(); j++){
+          columnValue += rows.get(j).charAt(i);
+        }
+        columns.add(columnValue);
       }
 
+      String message = "";
       String alpha = "abcdefghijklmnopqrstuvwxyz";
       String[] alphabet = alpha.split("");
-      int[] count = new int[26];
-      for(int i = 0; i < count.length; i++){ //loops 26 times for each letter
-        for(int j = 0; j < columns.size(); j++){ //loops through every string in input
-          //TURN ROWS INTO COLUMNS, THEN WORK ON THIS
+      for(int eachColumn = 0; eachColumn < columns.size(); eachColumn++){ //goes through each column
+        String columnValue = columns.get(eachColumn);
+        String[] columnValues = columnValue.split("");
+        int[] count = new int[26];
+        for(int eachLetter = 0; eachLetter < 26; eachLetter++){ //goes through each letter of the alphabet
+          for(int i = 0; i < columnValues.length; i++){ //goes through the string
+            if (columnValues[i].equals(alphabet[eachLetter])){
+              count[eachLetter] ++;
+            }
+          }
         }
+        int mostCommonAmt = 0;
+        String mostCommonLetter = "";
+        for(int j = 0; j < count.length; j++){
+          if (count[j] > mostCommonAmt){
+            mostCommonAmt = count[j];
+            mostCommonLetter = alphabet[j];
+          }
+        }
+        message += mostCommonLetter;
       }
-
+      System.out.println(message);
       sc.close();
 
     }catch(FileNotFoundException e){
