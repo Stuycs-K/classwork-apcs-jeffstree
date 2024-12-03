2dded588f05a25ca9ab883f5512876d772d0a19e
Jiefeng Ou <jou60@stuy.edu>
2024-12-03 11:16:20 -0500

    Started drawing of a fish

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,3 +1,5 @@
 public class ColorDemo{
-  
+  public static void go(int r,int c){
+    System.out.print("\u001b[" + r + ";" + c + "f");
+  }
 }
--- 2024-12-03-colors/Driver.java
+++ 2024-12-03-colors/Driver.java
@@ -1,5 +1,12 @@
 public class Driver{
   public static void main(String[] args){
-    
+    ColorDemo.go(1, 5);
   }
 }
+
+/* Drawing of a fish
+~-~-~-~-~-~-~ _______ ~-~-~-~-~-~-~-~-~
+-~-~-~-~-~-~-/       \-~-~-~-~-~-~-~-~-
+~-~-~-~-~-~-/         \-~-~-~-~-~-~-~-~
+-~-~-~-~-~--
+*/
