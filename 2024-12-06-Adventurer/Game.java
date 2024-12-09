4e7a555f24abfc5b2b168182917f09bbd17ff424
Jiefeng Ou <jou60@stuy.edu>
2024-12-09 11:00:52 -0500

    In the middle of doing player action

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -15,5 +15,22 @@ public class Game{
     String enemyInfo = enemy + ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName();
     System.out.println("You are: " + playerInfo);
     System.out.println("Your opponent is: " + enemyInfo);
+
+    boolean hasQuit = false;
+    while(!hasQuit){
+
+      boolean validAction = false;
+      while(!validAction){
+        System.out.println("Type: Type: (a)ttack / (sp)ecial / (su)pport / quit");
+        String playerAction = userInput.nextLine();
+        if(playerAction.equals(a) || playerAction.equals(attack)){
+          System.out.println("ATTACK HAPPENS");
+          validAction = true;
+        }
+        //ADD WHAT HAPPENS WHEN PLAYER CHOOSES SPECIAL, SUPPORT, OR QUIT
+      }
+
+      String.out.println("ENEMY DOES A THING");
+    }
   }
 }
