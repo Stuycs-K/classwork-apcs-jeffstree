545c9b17e0dfc423538f308a1d08a0198f372185
Jiefeng Ou <jou60@stuy.edu>
2024-12-09 10:54:39 -0500

    Added player, playerInfo, enemy, and enemyInfo

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -8,5 +8,12 @@ public class Game{
     String username = userInput.nextLine();
     System.out.println("Username is: " + username);
 
+    Adventurer player = new Warrior(username);
+    Adventurer enemy = new CodeWarrior("Emerie the Enemy");
+
+    String playerInfo = player + ", " + player.getHP() + "/" + player.getmaxHP() + " HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName();
+    String enemyInfo = enemy + ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName();
+    System.out.println("You are: " + playerInfo);
+    System.out.println("Your opponent is: " + enemyInfo);
   }
 }
